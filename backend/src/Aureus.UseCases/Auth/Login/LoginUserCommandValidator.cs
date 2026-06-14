f4b73e172a05a5320e6313db6652eb7ac8780494
Luka <hikmatovluka@gmail.com>
2026-06-14 16:44:12 +0300

    [test] move invalid email check to validator test, use strict email regex

--- backend/src/Aureus.UseCases/Auth/Login/LoginUserCommandValidator.cs
+++ backend/src/Aureus.UseCases/Auth/Login/LoginUserCommandValidator.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FluentValidation;
 using Aureus.UseCases.Validation;
 
@@ -5,12 +6,16 @@ namespace Aureus.UseCases.Auth.Login;
 
 internal sealed class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
 {
+    private static readonly Regex EmailRegex = new(
+        @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$",
+        RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
     public LoginUserCommandValidator()
     {
         RuleFor(x => x.Email)
             .NotEmpty()
             .MaximumLength(InputLimits.EmailMaxLength)
-            .EmailAddress();
+            .Matches(EmailRegex);
 
         RuleFor(x => x.Password)
             .NotEmpty()
--- backend/src/Aureus.UseCases/Auth/Register/Start/StartRegistrationCommandValidator.cs
+++ backend/src/Aureus.UseCases/Auth/Register/Start/StartRegistrationCommandValidator.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using FluentValidation;
 using Aureus.UseCases.Validation;
 
@@ -5,11 +6,15 @@ namespace Aureus.UseCases.Auth.Register.Start;
 
 internal sealed class StartRegistrationCommandValidator : AbstractValidator<StartRegistrationCommand>
 {
+    private static readonly Regex EmailRegex = new(
+        @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$",
+        RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
     public StartRegistrationCommandValidator()
     {
         RuleFor(x => x.Email)
             .NotEmpty()
             .MaximumLength(InputLimits.EmailMaxLength)
-            .EmailAddress();
+            .Matches(EmailRegex);
     }
 }
